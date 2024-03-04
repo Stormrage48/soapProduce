bfb9d22267bd4ec446f96f0dbfee0758310812a0
f.albay <f.albay@cekino.com>
2024-03-04 14:02:42 +0300

    Initial commit

---
+++ src/main/java/com/example/soapProduce/SoapProduceApplication.java
@@ -0,0 +1,13 @@
+package com.example.soapProduce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapProduceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapProduceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/soapProduce/SoapProduceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soapProduce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapProduceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
